9fe4d1439946a06209e7c0e3b629e8d20eacbef1
Marty Mooney <techno.mooney@gmail.com>
2019-07-23 01:03:17 -0500

    added  some nice things for it to run in a terminal with just hving it double clicked
    added file access exception handler

--- test_and_learn_autpmation_windows/FileListingsExcelSheets.py
+++ test_and_learn_autpmation_windows/FileListingsExcelSheets.py
@@ -1,4 +1,4 @@
-import os,glob,datetime,xlsxwriter,subprocess
+import os,glob,datetime,xlsxwriter,subprocess,time
 
 
 def main():
@@ -10,24 +10,31 @@ def main():
     #     print(filename)
     #     print(filedata)
     save_xls(path,data)
-
-
+    restart = input('\n\ndo you want to do another directory? to exit press \'n\' otherwise just press enter: ').lower()
+    if restart != 'n':
+        main()
+    else:
+        print('thanks for using the program made by Mary Mooney')
+        time.sleep(5)
+        quit()
 
 def setup():
     if os.path.exists('./do.not.delete.done_setup') is False:
         output = subprocess.run(['python','pip','install','--user','xlsxwriter'],capture_output=True).stdout.decode()
         out= open('do.not.delete.done_setup','w')
         out.write(output)
-        input('setup needed to be done, i installed xlsxwriter to use for this program... the file called \'do.not.del'
-              'ete.done_setup\' \nis there to tell thss proggram the setup was done already and theere is no need to do '
-              'it again! do not remove it unless \nyou want to rthe setup function again... \n\n press enter to conttinue')
+        input(f'setup needed to be done, i installed xlsxwriter to use for this program... the file called \'do.not.del'
+              f'ete.done_setup\' \nis there to tell thss proggram the setup was done already and theere is no need to do '
+              f'it again! do not remove it unless \nyou want to rthe setup function again... \n\n press enter to conttinue')
 
 def win_files(path):
     file_globs = glob.glob(path+r'\**\*',recursive=True)
     return file_globs
 
 def save_xls(path,data):
-    filename= str(path).replace("\\",'_').replace(':','')+f"---{str(datetime.datetime.now()).replace(':','..')}.xlsx"
+    path_append= str(os.path.expanduser('~'))+r'\Documents\.'
+    filename= path_append+str(path).replace("\\",'_').replace(':','')+f"---{str(datetime.datetime.now()).replace(':','..')}.xlsx"
+    print(f'the file will be saved in {path_append}')
 
     wb = xlsxwriter.Workbook(filename)
     worksheet = wb.add_worksheet('Files and attributes')
@@ -60,8 +67,12 @@ def path_check(path,check=None):
 def make_file_attri_dict(globbed_files):
     data={}
     data['file name'] = ['file path','file size in MB','last access time','Creation time']
-    for item in globbed_files:
-        data[os.path.basename(item)] = [item,(os.stat(item).st_size/1024)/1024, str(datetime.datetime.fromtimestamp(os.stat(item).st_atime)),str(datetime.datetime.fromtimestamp(os.stat(item).st_ctime))]
+    try:
+        for item in globbed_files:
+            data[os.path.basename(item)] = [item,(os.stat(item).st_size/1024)/1024, str(datetime.datetime.fromtimestamp(os.stat(item).st_atime)),str(datetime.datetime.fromtimestamp(os.stat(item).st_ctime))]
+    except Exception as err:
+        print(err)
+
     return data
 
 
