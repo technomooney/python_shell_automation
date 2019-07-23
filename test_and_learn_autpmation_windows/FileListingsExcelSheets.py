e09561ee686d5a3ead6ff8297004f9babc8591e3
Marty Mooney <techno.mooney@gmail.com>
2019-07-23 00:09:26 -0500

    small tweeks and got the file generated

--- test_and_learn_autpmation_windows/FileListingsExcelSheets.py
+++ test_and_learn_autpmation_windows/FileListingsExcelSheets.py
@@ -1,33 +1,50 @@
-import os,subprocess,re,glob,datetime
+import os,glob,datetime,xlsxwriter,subprocess
+
 
 def main():
+    setup()
     path = get_paths()
     globs= win_files(path)
     data = make_file_attri_dict(globs)
-    for (file,attri_list) in data.items():
-        print(file)
-        print(attri_list)
+    # for (filename,filedata) in data.items():
+    #     print(filename)
+    #     print(filedata)
+    save_xls(path,data)
 
 
+
+def setup():
+    if os.path.exists('./do.not.delete.done_setup') is False:
+        output = subprocess.run(['python','pip','install','--user','xlsxwriter'],capture_output=True).stdout.decode()
+        out= open('do.not.delete.done_setup','w')
+        out.write(output)
+        input('setup needed to be done, i installed xlsxwriter to use for this program... the file called \'do.not.del'
+              'ete.done_setup\' \nis there to tell thss proggram the setup was done already and theere is no need to do '
+              'it again! do not remove it unless \nyou want to rthe setup function again... \n\n press enter to conttinue')
 
 def win_files(path):
     file_globs = glob.glob(path+r'\**\*',recursive=True)
-    print(file_globs)
     return file_globs
 
+def save_xls(path,data):
+    filename= str(path).replace("\\",'_').replace(':','')+f"---{str(datetime.datetime.now()).replace(':','..')}.xlsx"
 
-#
-# def test(path):
-#     glob_var=glob.glob(path + r'\**\*', recursive=True)
-#     print(glob_var)
-#     for item in glob_var:
-#         print(datetime.datetime.fromtimestamp(os.stat(item).st_atime), os.stat(item).st_size)
-
-
-
+    wb = xlsxwriter.Workbook(filename)
+    worksheet = wb.add_worksheet('Files and attributes')
+    number= wb.add_format({'num_format':'#,##0.###'})
+    row = 0
+    col = 0
+    for (file,filedata) in data.items():
+        temp= col+1
+        worksheet.write(row,col,file)
+        for item in filedata:
+            worksheet.write(row,temp,item,number)
+            temp += 1
+        row += 1
+    wb.close()
 
 def get_paths():
-    path=input('enter the path: ')
+    path=input('enter the path you want to index into an excel file: ')
     path = path_check(path , check=1)
     return path
 
@@ -42,14 +59,15 @@ def path_check(path,check=None):
 
 def make_file_attri_dict(globbed_files):
     data={}
+    data['file name'] = ['file path','file size in MB','last access time','Creation time']
     for item in globbed_files:
-        data[item] = [(os.stat(item).st_size/1024), str(datetime.datetime.fromtimestamp(os.stat(item).st_atime))]
+        data[os.path.basename(item)] = [item,(os.stat(item).st_size/1024)/1024, str(datetime.datetime.fromtimestamp(os.stat(item).st_atime)),str(datetime.datetime.fromtimestamp(os.stat(item).st_ctime))]
     return data
 
 
 main()
 
-# TODO: get openxls or XlsxWriter and test how to make the final file
-# todo: be sure to make the file names of the xlsx file based off the time it was run and the path name by replacing
-#  the /s with -s
+# todo: make an empty filder sheet
+# todo: make it look nicer....
+
 
